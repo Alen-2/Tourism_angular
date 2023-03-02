ca6bded1361fc5ddd0eb1d66a2efde28071a9643
Alen-2 <alenkoshypramod@gmail.com>
2023-03-02 11:46:05 +0530

    first commit

---
+++ src/assets/data.ts
@@ -0,0 +1,85 @@
+export const districts =[
+  {
+    image:"https://i.ytimg.com/vi/3Cjc5dMj5ZI/mqdefault.jpg",
+    name:"Alappuzha",
+    details:"Alappuzha, famous for its boat races, beaches, marine products and coir industry, is a world renowned backwater tourist destination of India. Kuttanad, Alappuzha backwaters and Alappuzha beach are the must-see tourist attractions in the district.",
+  },
+
+  {
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDCwr58BQCauyeREECNX9A6zlJIyqonmravw&usqp=CAU",
+    name:"Ernakulam",
+    details:"Situated on beach-side, Kochi is a bustling commercial port with a trading history that dates back to at least 600 years, and is till today the financial capital of Kerala. This cosmopolitan town has upmarket stores, art galleries and some of the finest heritage accommodations.",
+  },
+
+  {
+    image:"https://images.unsplash.com/photo-1610449859460-46d4265ff8fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aWR1a2tpfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+    name:" Idukki",
+    details:"This beautiful High range district of Kerala is geographically known for its Mountainous Hills and Dense Forests. For the people of Kerala, Idukki is always associated with Power Generation.",
+  },
+
+  {
+    image:"https://i.ytimg.com/vi/zHeO6i0-JAo/maxresdefault.jpg",
+    name:"Kannur",
+    details:"Kannur is famous for its beaches, lush green forests, and temples. The place is often termed as one of the perfect places for a vacation. Also, Kannur is known for its culture and history.",
+  },
+
+  {
+    image:"https://theprimetime.in/wp-content/uploads/2021/11/Kasaragod-Kerala.jpg",
+    name:"Kasaragod",
+    details:"The northernmost district of Kerala, Kasaragod is famous for its handlooms and its 293 kilometre long coastline. With its cultural uniqueness, the place is known as the land of gods. Rich in history, Kasaragod is home to the largest and best preserved fort in the State - Bekal.",
+  },
+
+  {
+    image:"https://i.ytimg.com/vi/Ngy2hLzrkxw/maxresdefault.jpg",
+    name:"Kollam",
+    details:"Kollam is famous for its varied modern industries including cashew nut processing and research, fisheries, etc. Its association with the renowned Ashtamudi Lake makes it the gateway to the magnificent backwaters of Kerala.",
+  },
+
+  {
+    image:"https://www.keralatourism.org/images/ebooks/images/kottayam-ebrochure1.jpg",
+    name:"Kottayam",
+    details:"Kottayam is also called as “Akshara Nagari” which means the “city of letters” considering its contribution to print media and literature.",
+  },
+
+  {
+    image:"https://i.ytimg.com/vi/FlbpQ-STv48/maxresdefault.jpg",
+    name:"Kozhikode",
+    details:"Kozhikode is one of the best cities in Kerala. Endowed with lush green countrysides, serene beaches, historic sites, wildlife sanctuaries, rivers, and hills, this city enchants both its inhabitants and its guests. With its unique culture and friendly ambiance, Kozhikode is a wondrous destination for all.",
+  },
+
+  {
+    image:"https://hellovisit.in/uploads/0000/1/2022/10/06/travel-guide-for-malappuram.png",
+    name:"Malappuram",
+    details:"Malappuram has made numerous contributions to the cultural heritage of Kerala. A famous centre for Hindu-Vedic learning and Islamic philosophy, the temples and mosques of this region are well known for their spectacular festivals.",
+  },
+
+  {
+    image:"https://www.tourmyindia.com/blog//wp-content/uploads/2021/11/How-to-Reach-Pallakad-Kerala.jpg",
+    name:"Palakkad",
+    details:"Not only natural attractions there are many made attractions in Palakkad. Palakkad Fort is one such place. Situated at the middle of the city the fort welcomes you to this wonderful place. Adjacent to the the fort, there is a children's park and an open air stadium called Rappadi.",
+  },
+
+  {
+    image:"https://i.ytimg.com/vi/4ubGE44Fmbg/maxresdefault.jpg",
+    name:"Pathanamthitta",
+    details:"Situated near the Western Ghats and bordered by the hills, Pathanamthitta district is a treat to eyes with its vast unending stretches of forests, rivers and rural landscapes. Blessed by nature, the district is famous for its scenic beauty, fairs and festivals.",
+  },
+
+  {
+    image:"https://i.ytimg.com/vi/dBSpUD55_-U/maxresdefault.jpg",
+    name:"Thiruvananthapuram",
+    details:"Trivandrum is popular for the Padmanabhaswamy Temple, backwaters, and beaches. The place is known for its lively vibe and serene landscapes",
+  },
+
+  {
+    image:"https://i.ytimg.com/vi/Oofhj58OXIc/maxresdefault.jpg",
+    name:"Thrissur",
+    details:"Thrissur is known as the cultural capital of Kerala, and the land of Poorams. The district is known for its ancient temples, churches, and mosques. Thrissur Pooram is the most colourful temple festival in Kerala",
+  },
+
+{
+  image:"https://i.ytimg.com/vi/kF-bvOELf68/maxresdefault.jpg",
+  name:"Wayanad",
+  details:"This popular town is famous for wildlife and its spice plantations. Wayanad is part of a forest reserve, located on the border of Tamil Nadu and Kerala. Because of it wild green coverage it offers great scenic beauty along with the experience of seeing wildlife.",
+}
+]
